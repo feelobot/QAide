de9ddb09d7232aa2dfbb029ad2b51c89425d8447
Felix Rodriguez <felix.a.rod@gmail.com>
2013-09-23 22:53:23 -0700

    working up to gitc

--- config.js
+++ config.js
@@ -5,14 +5,13 @@ config.github_pass = process.env.GITHUB_PASS || '';
 
 config.lighthouse_username = process.env.LIGHTHOUSE_USER || '';
 config.lighthouse_pass = process.env.LIGHTHOUSE_PASS || '';
+config.github_url = "https://github.com/br/"
+config.lighthouse_url = "https://bleacherreport.lighthouseapp.com/projects/6296/tickets/"
+config.shipit_dir = "~/code/shipit"
 
+config.projects = {
+	"api" : /https:\/\/github.com\/br\/api\/pull\/([0-9]+)/g,
+	"breport" : /https:\/\/github.com\/br\/breport\/pull\/([0-9]+)/g
+}
 module.exports = config;
 
-var github_url = "https://github.com/br/"
-var ticket_url = "https://bleacherreport.lighthouseapp.com/projects/6296/tickets/"
-var shipit_dir = "~/code/shipit"
-
-var pull_requests = {
-	/https:\/\/github.com\/br\/breport\/issues\/([0-9]+)/g,
-	/https:\/\/github.com\/br\/api\/issues\/([0-9]+)/g
-}
--- qaide.js
+++ qaide.js
@@ -4,7 +4,6 @@ var GitHubApi = require("github");
 var sys = require('sys');
 var exec = require('child_process').exec;
 var config = require('./config')
-
 var github = new GitHubApi({
     // required
     version: "3.0.0",
@@ -26,40 +25,54 @@ github.authenticate({
   password: config.github_pass
 });
 
-var deploy = "cd  " + config.shipit_dir + " && " + deploy
+var deploy = "cd  " + config.shipit_dir + " && "
 
 qaide
   .version('0.0.1')
-  .option('-t, --tickets <tickets>' , 'the tickets to qa', list)
+  .option('-t, --ticket [number]' , 'the ticket to qa')
   .option('-e, --environment', 'Specify the staging environment to deploy to')
-  .option('-u, --user' , 'the user to deploy as')  
+  .option('-u, --user [value]' , 'the user to deploy as')  
   .option('-o, --options', 'other deploy options i.e. MIGRATIONS=1')
   .parse(process.argv);
 
-qaide.tickets.forEach(val,index,array) {
-	url = config.ticket_url + val
-	request(url, lighthouse_authenticate, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	    qaide.list.forEach(val,index,array) {
-	    	var pull = body.match(val).pop();
-	    }
-	    var pull = body.match(regex).pop();
-	    function puts(error, stdout, stderr) { sys.puts(stdout) }
-	    var pullNumber = pull.slice(-4);
-	    pull_requests_to_qa += pullNumber
-	  }
-	  else console.log(response.statusCode + " Error for " + url)
-	  
-	  github.pullRequests.get({
-	    user: "br",
-	    repo: "breport",
-	    number: pullNumber
-		},
-	  function(err, res) {
-		  var branch = res["base"]["ref"]
-		  console.log(branch)
-	  });
-	});
+var ticket_url = config.lighthouse_url + qaide.ticket
+console.log(ticket_url);
+
+function puts(error, stdout, stderr) { 
+  sys.puts(stdout) 
+  sys.puts(stderr)
 }
 
+request(ticket_url, lighthouse_authenticate, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+  	if (body.match(config.projects["api"])) {
+      var last_api = body.match(config.projects["api"]).pop()
+      var branch = get_branch_name(last_api)
+      //exec("cd ~/code/breport/ && git checkout " + branch + " && git pull origin " + branch //+ " gitc qa " + pull_requests_num, puts);
+    }
+    if (body.match(config.projects["breport"])) {
+      var last_breport = body.match(config.projects["breport"]).pop()
+      pull_requests_num = last_breport.match(/[0-9]+/)[0]
+      console.log(pull_requests_num);
+      github.pullRequests.get({
+        user: "br",
+        repo: "breport",
+        number: pull_requests_num
+      },
+      function(err, res) {
+        var base_branch = res["base"]["ref"]
+        var head_branch = res["head"]["label"]
+        console.log(developer)
+        console.log(head);
 
+        console.log(branch);
+        var qa_branch = "qa_" + base_branch + "_" + qaide.user
+        exec("cd ~/Documents/code/breport/ && git checkout " + base_branch + " && git pull origin " + base_branch + " && git branch -D " + qa_branch " && git checkout -b " + qa_branch + " && git merge " + head_branch + " && git push -u origin " + qa_branch", puts);
+      });
+    }
+  	else { 
+  		console.log("Project Undefined");
+  	}
+  }
+ 	else console.log(response.statusCode + " Error for " + url)
+});	
